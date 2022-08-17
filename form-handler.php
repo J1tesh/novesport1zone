80281db6a882bcff34482f82e0e83f664aa33905
J1tesh <jiteshkumar0302@gmail.com>
2022-08-17 14:13:49 +0530

    collegetask

---
+++ form-handler.php
@@ -0,0 +1,29 @@
+<?php
+$name =$_POST['name'];
+$visitor_email =$_POST['email'];
+$subject =$_POST['subject'];
+$message =$_POST['Message'];
+
+$email_from='Novesport@.com';
+$email_subject='new form submission';
+
+$email_body="user Name:$name.\n".
+             "user email:$visitor_email.\n".
+              "subject:$subject.\n".
+               "user Message:$message.\n".;
+        
+ $to='jiteshkumar0302@gmail.com';
+ 
+ $headers="from: $email_from \r\n";
+ $headers="Reply-to: $visitor_email \r\n";
+   
+
+ mail($to,$email_subject,$email_body,$headers);
+ header("Location:contact .html.html");
+
+
+
+
+
+
+?>
